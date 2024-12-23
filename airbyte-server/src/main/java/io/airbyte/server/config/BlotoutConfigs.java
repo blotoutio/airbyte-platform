2a2192879fd2df3d39a30f7917d29aed5a31bf8d
azaveri7 <azaveri7@gmail.com>
2024-12-23 10:47:57 +0530

    indentation changes

--- airbyte-server/src/main/java/io/airbyte/server/config/BlotoutConfigs.java
+++ airbyte-server/src/main/java/io/airbyte/server/config/BlotoutConfigs.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020-2024 Airbyte, Inc., all rights reserved.
+ */
+
 package io.airbyte.server.config;
 
 import jakarta.inject.Singleton;
@@ -5,20 +9,4 @@ import jakarta.inject.Singleton;
 @Singleton
 public class BlotoutConfigs {
 
-    // Fetch from environment variables
-    public String getBlotoutBaseUrl() {
-        String blotoutBaseUrl = System.getenv("BLOTOUT_BASE_URL");
-        if (blotoutBaseUrl == null) {
-            throw new IllegalArgumentException("BLOTOUT_BASE_URL environment variable is not set.");
-        }
-        return blotoutBaseUrl;
-    }
-
-    public String getBlotoutAuthEndpoint() {
-        String blotoutAuthEndpoint = System.getenv("BLOTOUT_AUTH_ENDPOINT");
-        if (blotoutAuthEndpoint == null) {
-            throw new IllegalArgumentException("BLOTOUT_AUTH_ENDPOINT environment variable is not set.");
-        }
-        return blotoutAuthEndpoint;
-    }
 }
