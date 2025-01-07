eed51e1d92a82c39eae34306f499f35bdb9a84ee
azaveri7 <azaveri7@gmail.com>
2025-01-07 12:11:03 +0530

    adding debug messages

--- airbyte-server/src/main/java/io/airbyte/server/pro/AuthenticationFilter.java
+++ airbyte-server/src/main/java/io/airbyte/server/pro/AuthenticationFilter.java
@@ -44,6 +44,9 @@ public class AuthenticationFilter implements HttpFilter {
         }
 
         if (isEdgeTagBasedAuthentication(originHeader)) {
+            LOGGER.debug("authorizationHeader -> " + authorizationHeader);
+            LOGGER.debug("originHeader -> " + originHeader);
+            LOGGER.debug("teamIdHeader -> " + teamIdHeader);
             // Use Mono.defer to ensure this operation is handled asynchronously
             return Mono.defer(() -> blotoutAuthentication.validateEdgeTagBasedAuthentication(originHeader, authorizationHeader, teamIdHeader)
                     .flatMap(valid -> {
@@ -63,6 +66,9 @@ public class AuthenticationFilter implements HttpFilter {
                     })
             );
         } else if (isTokenBasedAuthentication(authorizationHeader)) {
+            LOGGER.debug("authorizationHeader -> " + authorizationHeader);
+            LOGGER.debug("originHeader -> " + originHeader);
+            LOGGER.debug("teamIdHeader -> " + teamIdHeader);
             // Extract the token (remove "Bearer " prefix)
             String token = authorizationHeader.substring(AUTHENTICATION_SCHEME.length()).trim();
             return Mono.defer(() -> blotoutAuthentication.validateToken(token)
@@ -83,7 +89,7 @@ public class AuthenticationFilter implements HttpFilter {
                     })
             );
         } else {
-            LOGGER.error(" return from last else part ");
+            LOGGER.debug(" return from last else part ");
             return Mono.from(chain.proceed(request));  // Continue without any authentication logic
         }
     }
