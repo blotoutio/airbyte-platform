312865d5c6f500649fb2d42b2a46e5dabcc81bfd
azaveri7 <azaveri7@gmail.com>
2025-01-09 11:43:44 +0530

    hard coding constants

--- airbyte-server/src/main/java/io/airbyte/server/config/BlotoutConfigs.java
+++ airbyte-server/src/main/java/io/airbyte/server/config/BlotoutConfigs.java
@@ -7,15 +7,25 @@ package io.airbyte.server.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import jakarta.inject.Singleton;
+import io.micronaut.context.annotation.Value;
 
 @Singleton
 public class BlotoutConfigs {
 
+  @Value("${blotout.baseUrl}")
+  private String blotoutBaseUrl;
+
+  @Value("${blotout.authEndpoint}")
+  private String blotoutAuthEndpoint;
+
   private static final Logger LOGGER = LoggerFactory.getLogger(BlotoutConfigs.class);
   // Fetch from environmental variables
   public String getBlotoutBaseUrl() {
-      String blotoutBaseUrl = System.getenv("BLOTOUT_BASE_URL");
-      LOGGER.warn("BLOTOUT_BASE_URL =====>" + blotoutBaseUrl);
+      String env_blotoutBaseUrl = System.getenv("BLOTOUT_BASE_URL");
+      LOGGER.warn("BLOTOUT_BASE_URL =====>" + env_blotoutBaseUrl);
+      if(Objects.nonNull(blotoutBaseUrl) && !blotoutBaseUrl.contains("https")) {
+          blotoutBaseUrl =  "https://" + blotoutBaseUrl;
+      }
       if(blotoutBaseUrl == null) {
           LOGGER.warn("BLOTOUT_BASE_URL env variable not found");
           throw new IllegalArgumentException("BLOTOUT_BASE_URL environment variable is not set.");
@@ -24,8 +34,8 @@ public class BlotoutConfigs {
   }
 
   public String getBlotoutAuthEndpoint() {
-      String blotoutAuthEndpoint = System.getenv("BLOTOUT_AUTH_ENDPOINT");
-      LOGGER.warn("BLOTOUT_AUTH_ENDPOINT =====>" + blotoutAuthEndpoint);
+      String env_blotoutAuthEndpoint = System.getenv("BLOTOUT_AUTH_ENDPOINT");
+      LOGGER.warn("BLOTOUT_AUTH_ENDPOINT =====>" + env_blotoutAuthEndpoint);
       if(blotoutAuthEndpoint == null) {
           LOGGER.warn("BLOTOUT_AUTH_ENDPOINT env variable not found");
           throw new IllegalArgumentException("BLOTOUT_BASE_URL environment variable is not set.");
